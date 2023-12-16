d17502bb017e833a29c0596f578a70215cea5763
DESKTOP-SAKB979\Bogdan <bogdanguk@gmail.com>
2023-12-17 01:27:33 +0200

    Added possibility to detect drag start gesture in FormElementsLibrary

--- FormElementsLibrary.py
+++ FormElementsLibrary.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PySide6.QtGui import QDragMoveEvent, QMouseEvent, QPixmap
+from PySide6.QtCore import Qt
 from enum import Enum
 import res.LibElementIcons_rc as LibElementIcons_rc
 
@@ -15,15 +16,28 @@ class FormElementsLibrary(QWidget):
         layout = QVBoxLayout()
 
         label_icon = QPixmap(":/libElements/labelIcon.png")
-        label_icon_label = QLabel()
-        label_icon_label.setPixmap(label_icon)
-        layout.addWidget(label_icon_label)
+        self.label_icon_label = QLabel()
+        self.label_icon_label.setPixmap(label_icon)
+        layout.addWidget(self.label_icon_label)
 
         line_edit_icon = QPixmap(":/libElements/lineEditIcon.png")
-        line_edit_icon_label = QLabel()
-        line_edit_icon_label.setPixmap(line_edit_icon)
-        layout.addWidget(line_edit_icon_label)
+        self.line_edit_icon_label = QLabel()
+        self.line_edit_icon_label.setPixmap(line_edit_icon)
+        layout.addWidget(self.line_edit_icon_label)
 
         layout.addStretch(1)
 
         self.setLayout(layout)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if event.button() == Qt.LeftButton :
+            self._dragStartPos = event.pos()
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if event.buttons() == Qt.LeftButton :
+            distance = (event.pos() - self._dragStartPos).manhattanLength()
+            if distance > QApplication.startDragDistance() :
+                self.startDrag()
+
+    def startDrag(self):
+        print("start drag")
